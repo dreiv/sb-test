006f5f6a9edc6e2f09c33b43c13410cbea7e3aa0
Andrei Voicu <dreivoicu@gmail.com>
2020-05-15 13:12:59 +0300

    created button lib

---
+++ projects/button/src/lib/button.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'ui-button',
+  template: `
+   <button [disabled]="disabled">{{text}}</button>
+  `,
+  styles: [
+  ]
+})
+export class ButtonComponent {
+  @Input() text: string;
+  @Input() disabled: boolean;
+}
